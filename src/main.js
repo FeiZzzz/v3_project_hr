d6e77358f3e7730c7e349ad607703b8b3df5d9c5
FeiZzzz <1552508702@qq.com>
2023-06-07 22:22:11 +0800

    安装 Ant design-vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+// 引入Ant组件库
+import AntD from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.less'
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+// 全局注册antD
+app.use(AntD)
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Layout from '@/view/layout/index.vue'
+import Layout from '../views/layout/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
