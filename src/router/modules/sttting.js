3104a73003da84da5844bcce53cbc55a914e245a
FeiZzzz <1552508702@qq.com>
2023-06-11 10:45:43 +0800

    角色管理的路由和页面

---
+++ src/router/modules/sttting.js
@@ -0,0 +1,19 @@
+import layout from '../../views/layout/index.vue'
+export default {
+  path: '/setting', // 路由地址
+  component: layout,
+  children: [
+    {
+      path: '', // 二级路由写个空 表示 它是默认的二级路由
+      component: () => import('../../views/settting/index.vue'),
+      meta: {
+        title: '公司设置',
+        icon: 'SettingOutlined'
+      }
+    }
+  ]
+
+  // 角色管理的路由
+}
+
+// 二级路由 path: '' /setting => layout-children
